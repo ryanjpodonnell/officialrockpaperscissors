a7b0d91367d8a6520b9ca7f286eb3ae01c11349a
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2014-05-11 14:41:14 -0700

    connectivity problems fixed

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ app.use(express.static('public'));
 
 var players = [];
 io.sockets.on('connection', function(socket) {
+  console.log("connection");
 	var player = socket.id;
 	players.push(player);
 	var id = players.indexOf(player)
   
   socket.on('disconnect', function() {
+    console.log("disconnection");
     // players.splice(id, 1);
     players.length = 0;
+    io.sockets.emit('endGame');
   });
   
   socket.emit('receive', { id: id });
